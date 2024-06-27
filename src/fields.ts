a9666ff6d88f0bc93815380ed745f0d02643f2a1
DavidQF555 <davidfeng2003@gmail.com>
2024-06-27 06:20:55 -0700

    improved field definitions

--- src/fields.ts
+++ src/fields.ts
@@ -1,5 +1,7 @@
 import { Anime, AnimeListStatus, DetailedAnime, DetailedManga, Manga, MangaListStatus, UserInfo } from './types.js';
 
+type Fields = { [key: string]: boolean | Fields | undefined }
+
 /**
  * Converts an object to a field string as specified by {@link https://myanimelist.net/apiconfig/references/api/v2#section/Common-parameters}
  *
@@ -10,10 +12,10 @@ import { Anime, AnimeListStatus, DetailedAnime, DetailedManga, Manga, MangaListS
  *
  * @returns a field string converted from the object
  */
-export default function parseFields(fields: object): string {
+export default function parseFields(fields: Fields): string {
 	const parts: Array<string> = [];
 	for(const key in fields) {
-		const value = fields[key as keyof object];
+		const value = fields[key as keyof Fields];
 		if(value) {
 			if(typeof value === 'object') {
 				const parsed: string = parseFields(value);
@@ -27,7 +29,7 @@ export default function parseFields(fields: object): string {
 	return parts.join(',');
 }
 
-export interface UserAnimeListFields extends AnimeFields {
+export interface UserAnimeListFields extends Fields, AnimeFields {
     list_status?: boolean | AnimeListStatusFields;
 }
 
@@ -43,7 +45,7 @@ export type DetailedAnimeFields = {
     [T in keyof DetailedAnime]?: boolean;
 }
 
-export interface UserMangaListFields extends MangaFields {
+export interface UserMangaListFields extends Fields, MangaFields {
     list_status?: boolean | MangaListStatusFields;
 }
 
